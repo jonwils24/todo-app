ce4b4cfea3e114be2ff5ecf75813232ee9b52244
Jonathan Wilson <wilsonwjonathan@gmail.com>
2022-07-04 11:28:32 -0700

    BTerraform backend config, provider, and main module

---
+++ terraform/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  version    = "~> 3.0"
+  region     = var.region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+module "static_site" {
+  source      = "./modules/static_site"
+  bucket_name = "tmp-todo-app"
+}
