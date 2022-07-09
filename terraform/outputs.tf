12864441495757803add4885a4d4aa4c9ee4a24d
Jonathan Wilson <wilsonwjonathan@gmail.com>
2022-07-09 16:48:47 -0700

    dtemp disable static site module outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
-output "website_bucket_name" {
-  value = module.static_site.website_bucket_name
-}
+# output "website_bucket_name" {
+#   value = module.static_site.website_bucket_name
+# }
 
-output "cloudfront_id" {
-  value = module.static_site.cloudfront_id
-}
+# output "cloudfront_id" {
+#   value = module.static_site.cloudfront_id
+# }
