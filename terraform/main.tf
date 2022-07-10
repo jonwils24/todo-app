d90526d56553e47e056ed31701e1e70760b0ce1f
Jonathan Wilson <wilsonwjonathan@gmail.com>
2022-07-10 07:58:40 -0700

    added static_site and todos_api modules to main TF module

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,3 +3,17 @@ provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
+
+locals {
+  name = "jw-todos-app"
+}
+
+module "static_site" {
+  source      = "./modules/static_site"
+  bucket_name = local.name
+}
+
+module "todos_api" {
+  source = "./modules/api"
+  name = local.name
+}
